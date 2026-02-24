164b8b0e60f0fca1888f028bdf3843d175569bff
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2026-02-24 10:16:11 +0530

    Update index.js

--- AgroRegister-Backend/src/db/index.js
+++ AgroRegister-Backend/src/db/index.js
@@ -4,10 +4,11 @@ const connectDB = async () => {
   try {
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+      connectTimeout: 20000, // 20 seconds (helps debugging timeout)
     });
 
     console.log("✅ MySQL database connected successfully!");
@@ -19,3 +20,25 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+// import mysql from "mysql2/promise";
+
+// const connectDB = async () => {
+//   try {
+//     const connection = await mysql.createConnection({
+//       host: process.env.DB_HOST,
+//       port: process.env.DB_PORT,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASS,
+//       database: process.env.DB_NAME,
+//     });
+
+//     console.log("✅ MySQL database connected successfully!");
+//     return connection;
+//   } catch (error) {
+//     console.error("❌ MySQL DB connection failed !!!", error);
+//     throw error;
+//   }
+// };
+
+// export default connectDB;
