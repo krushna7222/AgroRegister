565e6f99be3f422f6b1350a1d7c257bd3f9e8890
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2026-02-24 09:59:47 +0530

    Update connection.js

--- AgroRegister-Backend/src/db/connection.js
+++ AgroRegister-Backend/src/db/connection.js
@@ -9,9 +9,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
-  }
+  },
 );
 
 export default sequelize;
